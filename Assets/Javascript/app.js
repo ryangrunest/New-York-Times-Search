4f8b575b49e422fb303d0b16aece8b553303fb47
Ryan Grunest <ryangrunest45@gmail.com>
2019-02-23 13:44:50 -0800

    push for nash

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,26 +1,37 @@
 /* API Key = IZXj7BjN7oauLYCKocfKCTNV6moMqX1K */
 var search;
 var numberRecords;
-var begin_date;
-var end_date;
+var begin_date = '';
+var end_date = '';
 
 
 $("#search-button").on("click", function() {
     var search = $("#search").val();
-    var begin_date = $("#start").val();
-    var end_date = $("#end").val();
+    if ($('#start').val() != '') {
+    	var begin_date = `&begin_date=${$("#start").val()}0101`;
+    }
+    if ($('#end').val() != '') {
+    	var end_date = `&end_date=${$("#end").val()}0101`;
+    }
+    
     var numberRecords = $('#number').val();
 
     //API query link
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+"&"+begin_date+"&"+end_date+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K"
+    // 
+    if(begin_date)
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+begin_date+end_date+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K";
 
     //ajax function to pull from API
     $.ajax({
       url: queryURL,
       method: "GET",
-      success: function() {
+      success: function(query) {
       	// searchFunc();
-      	console.log(response);
+
+      	console.log(query);
+      	var data = query.response.docs[0];
+      	var header = $('<div>');
+      	console.log(data);
       },
       error: function() {
       	alert('Error: Articles Not Found');
