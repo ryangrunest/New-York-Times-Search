2739067ace8ed111be05ac71583aeab3ea5915a8
Ryan Grunest <ryangrunest45@gmail.com>
2019-02-23 14:26:30 -0800

    push

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,11 +1,15 @@
 /* API Key = IZXj7BjN7oauLYCKocfKCTNV6moMqX1K */
+
+// init search variables
 var search;
-var numberRecords;
-var begin_date = '';
-var end_date = '';
+var numberRecords = 1;
+var begin_date;
+var end_date;
+var searchStr = '';
 
-
+// search button on click functionality
 $("#search-button").on("click", function() {
+	// if dates are not empty, store value from user input
     var search = $("#search").val();
     if ($('#start').val() != '') {
     	var begin_date = `&begin_date=${$("#start").val()}0101`;
@@ -14,24 +18,41 @@ $("#search-button").on("click", function() {
     	var end_date = `&end_date=${$("#end").val()}0101`;
     }
     
-    var numberRecords = $('#number').val();
+    // set number of records to retrieve
+    if ($('#number').val() != undefined) {
+    	var numberRecords = Int($('#number').val());
+    }
+    
 
-    //API query link
-    // 
-    if(begin_date)
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+begin_date+end_date+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K";
+
+
+    if (search != undefined){
+    	searchStr = searchStr+search;
+    }
+    if (begin_date != undefined){
+   		searchStr = searchStr+begin_date;
+    }
+    if (end_date != undefined){
+    	searchStr = searchStr+end_date;
+    }
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchStr+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K";
 
     //ajax function to pull from API
     $.ajax({
       url: queryURL,
       method: "GET",
       success: function(query) {
-      	// searchFunc();
+      	if (numberRecords != undefined) {
+      		for (var i = 0; i < numberRecords; i++) {
 
+      		}
+      	}
       	console.log(query);
       	var data = query.response.docs[0];
       	var header = $('<div>');
       	console.log(data);
+
       },
       error: function() {
       	alert('Error: Articles Not Found');
