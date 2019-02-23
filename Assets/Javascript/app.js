5cea5f3164b297c1bafd2f4128695a036e66e0c7
Ryan Grunest <ryangrunest45@gmail.com>
2019-02-23 15:27:35 -0800

    cleaned up js codebase

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -11,35 +11,33 @@ $("#search-button").on("click", function() {
     var search = $("#search").val();
     if ($('#start').val() != '') {
       var begin_date = `&begin_date=${$("#start").val()}0101`;
-    }
+    };
     if ($('#end').val() != '') {
       var end_date = `&end_date=${$("#end").val()}0101`;
-    }
+    };
     
     // set number of records to retrieve
     // if ($("number").val()!=0){
     var numberRecords = $('#number').val();
 
-
-
     if (search != undefined){
-      searchStr = searchStr+search;
+      searchStr = searchStr + search;
     };
     if (begin_date != undefined){
-      searchStr = searchStr+begin_date;
+      searchStr = searchStr + begin_date;
     };
     if (end_date != undefined){
-      searchStr = searchStr+end_date;
+      searchStr = searchStr + end_date;
     };
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchStr+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K";
+    var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchStr}&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K`;
     $.ajax({
       url: queryURL,
       method: "GET",
+      // if successfully grabbed object for 
       success: function(query) {
-        // searchFunc();
-        if (numberRecords !=undefined){
-          for (var i=0; i<numberRecords; i++){
+        if (numberRecords != undefined){
+          for (var i = 0; i < numberRecords; i++){
             var data = query.response.docs[i];
             var articleDiv = $('<div>');
             var headlineDiv = $('<div>');
@@ -62,13 +60,8 @@ $("#search-button").on("click", function() {
              
             articleDiv.append(headlineDiv, authorDiv, sectionDiv, artDateDiv, urlDiv);
             $('#articles').append(articleDiv);
-        
-          }
-
-          }
-
-        
-        
+          };
+        };        
       },
       error: function() {
         alert('Error: Articles Not Found');
@@ -76,7 +69,7 @@ $("#search-button").on("click", function() {
     });
   });// init search variables
 var search;
-var numberRecords=1;
+var numberRecords = 1;
 var begin_date;
 var end_date;
 var searchStr = '';
@@ -87,10 +80,10 @@ $("#search-button").on("click", function() {
     var search = $("#search").val();
     if ($('#start').val() != '') {
       var begin_date = `&begin_date=${$("#start").val()}0101`;
-    }
+    };
     if ($('#end').val() != '') {
       var end_date = `&end_date=${$("#end").val()}0101`;
-    }
+    };
     
     // set number of records to retrieve
     // if ($("number").val()!=0){
@@ -99,13 +92,13 @@ $("#search-button").on("click", function() {
 
 
     if (search != undefined){
-      searchStr = searchStr+search;
+      searchStr = searchStr +. search;
     };
     if (begin_date != undefined){
-      searchStr = searchStr+begin_date;
+      searchStr = searchStr + begin_date;
     };
     if (end_date != undefined){
-      searchStr = searchStr+end_date;
+      searchStr = searchStr + end_date;
     };
 
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchStr+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K";
@@ -138,13 +131,8 @@ $("#search-button").on("click", function() {
              
             articleDiv.append(headlineDiv, authorDiv, sectionDiv, artDateDiv, urlDiv);
             $('#articles').append(articleDiv);
-        
-          }
-
-          }
-
-        
-        
+          };
+        };  
       },
       error: function() {
         alert('Error: Articles Not Found');
