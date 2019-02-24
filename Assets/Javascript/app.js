39355f367c3f2cef878444f6eb2dee116159e407
Ryan Grunest <ryangrunest45@gmail.com>
2019-02-23 16:21:44 -0800

    cleaned up js

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,9 +1,13 @@
 // init search variables
 var search;
-var numberRecords=1;
+var numberRecords = 10;
 var begin_date;
 var end_date;
 var searchStr = '';
+
+$('select[id=number]').on('change', function() {
+  numberRecords = parseInt($(this).val());
+})
 
 // search button on click functionality
 $("#search-button").on("click", function() {
@@ -17,8 +21,7 @@ $("#search-button").on("click", function() {
     };
     
     // set number of records to retrieve
-    // if ($("number").val()!=0){
-    var numberRecords = $('#number').val();
+    numberRecords = $('#number').attr('value');
 
     if (search != undefined){
       searchStr = searchStr + search;
@@ -36,103 +39,31 @@ $("#search-button").on("click", function() {
       method: "GET",
       // if successfully grabbed object for 
       success: function(query) {
-        if (numberRecords != undefined){
-          for (var i = 0; i < numberRecords; i++){
-            var data = query.response.docs[i];
-            var articleDiv = $('<div>');
-            var headlineDiv = $('<div>');
-            var authorDiv = $('<div>');
-            var sectionDiv = $('<div>');
-            var artDateDiv = $('<div>');
-            var urlDiv = $('<a>');
+        console.log(query);
+        var data = query.response;
+          for (var i = 0; i < numberRecords; i++) {
+          var articleDiv = $('<div>').attr('class', 'card-body col-12');
+          var headlineDiv = $('<h4>');
+          var authorDiv = $('<div>');
+          var sectionDiv = $('<div>');
+          var artDateDiv = $('<div>');
+          var urlDiv = $('<a>');
                 
-            var headlineText = data.headline.main;
-            var authorText = data.byline.original;
-            var sectionText = data.section_name;
-            var artDateText = data.pub_date;
-            var urlText = data.web_url;
-    
-            headlineDiv.text(headlineText);
-            authorDiv.text(authorText);
-            sectionDiv.text(sectionText);
-            artDateDiv.text(artDateText);
-            urlDiv.attr("href",urlText).text(urlText);
-             
-            articleDiv.append(headlineDiv, authorDiv, sectionDiv, artDateDiv, urlDiv);
-            $('#articles').append(articleDiv);
-          };
-        };        
-      },
-      error: function() {
-        alert('Error: Articles Not Found');
-      }
-    });
-  });// init search variables
-var search;
-var numberRecords = 1;
-var begin_date;
-var end_date;
-var searchStr = '';
-
-// search button on click functionality
-$("#search-button").on("click", function() {
-  // if dates are not empty, store value from user input
-    var search = $("#search").val();
-    if ($('#start').val() != '') {
-      var begin_date = `&begin_date=${$("#start").val()}0101`;
-    };
-    if ($('#end').val() != '') {
-      var end_date = `&end_date=${$("#end").val()}0101`;
-    };
-    
-    // set number of records to retrieve
-    // if ($("number").val()!=0){
-    var numberRecords = $('#number').val();
-
-
-
-    if (search != undefined){
-      searchStr = searchStr +. search;
-    };
-    if (begin_date != undefined){
-      searchStr = searchStr + begin_date;
-    };
-    if (end_date != undefined){
-      searchStr = searchStr + end_date;
-    };
-
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchStr+"&api-key=IZXj7BjN7oauLYCKocfKCTNV6moMqX1K";
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-      success: function(query) {
-        // searchFunc();
-        if (numberRecords !=undefined){
-          for (var i=0; i<numberRecords; i++){
-            var data = query.response.docs[i];
-            var articleDiv = $('<div>');
-            var headlineDiv = $('<div>');
-            var authorDiv = $('<div>');
-            var sectionDiv = $('<div>');
-            var artDateDiv = $('<div>');
-            var urlDiv = $('<a>');
-                
-            var headlineText = data.headline.main;
-            var authorText = data.byline.original;
-            var sectionText = data.section_name;
-            var artDateText = data.pub_date;
-            var urlText = data.web_url;
-    
-            headlineDiv.text(headlineText);
-            authorDiv.text(authorText);
-            sectionDiv.text(sectionText);
-            artDateDiv.text(artDateText);
-            urlDiv.attr("href",urlText).text(urlText);
-             
-            articleDiv.append(headlineDiv, authorDiv, sectionDiv, artDateDiv, urlDiv);
-            $('#articles').append(articleDiv);
-          };
-        };  
+          var headlineText = docs[i].data.headline.main;
+          var authorText = docs[i].data.byline.original;
+          var sectionText = docs[i].data.section_name;
+          var artDateText = docs[i].data.pub_date;
+          var urlText = docs[i].data.web_url;
+  
+          headlineDiv.text(headlineText);
+          authorDiv.text(authorText);
+          sectionDiv.text(sectionText);
+          artDateDiv.text(`Published: ${artDateText}`);
+          urlDiv.attr("href", urlText).text(urlText);
+           
+          articleDiv.append($('<hr>'), headlineDiv, authorDiv, sectionDiv, artDateDiv, urlDiv, $('<hr>'));
+          $('#articles').append(articleDiv);
+          };     
       },
       error: function() {
         alert('Error: Articles Not Found');
